fdb5a47c7a3960df280fcb921e8d02b49fbe3165
Jose Antonio <josementi@gmail.com>
2025-01-14 00:01:20 +0100

    More changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,6 @@ processor = "processor:integration"
 def base_publisher_url():
     return "http://localhost:8000"
 
-
 @pytest.fixture(scope="session", autouse=True)
 def images(request):
     # Build the images in parallel to save time
@@ -28,17 +27,16 @@ def images(request):
     # Build the base images as they'd be deployed in production
     create_docker_image("./order-processor", processor_base)
     create_docker_image("./order-publisher", publisher_base)
-
+ 
     # This uses the base images and extends them to include test-specific dependencies. In this case... just Dapr
     # but it could also include other things such as az cli or test volumes for sample payloads
-    create_docker_image("docker-images/order-processor", processor)
-    create_docker_image("docker-images/order-publisher", publisher)
+    create_docker_image("./tests/docker-images/dapr", publisher_base)
+    create_docker_image("./tests/docker-images/order-processor", processor)
+    create_docker_image("./tests/docker-images/order-publisher", publisher)
 
-
-@pytest.fixture
-def containers(images):
-    # This function structure showcases the workflow of the test containers. We can see how the dependencies are passed
-    # "top down" with the network at the root. When the execution finishes this entire stack of nested 'withs' will
+@pytest.fixture(scope="session", autouse=True)
+def containers(request, images):
+    # Build the images in parallel to save time def create_docker_image(path: str, tag: str) -> DockerImage: return DockerImage(path=path, tag=tag).build() # Build the base images as they'd be deployed in production create_docker_image("./order-processor", processor_base) create_docker_image("./order-publisher", publisher_base) create_docker_image("./tests/docker-images/dapr", "dapr:integration") # This uses the base images and extends them to include test-specific dependencies. In this case... just Dapr # but it could also include other things such as az cli or test volumes for sample payloads create_docker_image("./tests/docker-images/order-processor", processor) create_docker_image("./tests/docker-images/order-publisher", publisher) @pytest.fixture def containers(images): # This function structure showcases the workflow of the test containers. We can see how the dependencies are passed # "top down" with the network at the root. When the execution finishes this entire stack of nested 'withs' will
     # unravel and free up resources in order, essentially cleaning up the test environment for a new run.
     with Network() as network:
         with (RedisContainer(image="redis:7.4.2-alpine").with_network(network).with_name("redis").with_bind_ports(6379, 6380)) as redis_container:
@@ -49,5 +47,15 @@ def containers(images):
                         "publisher": publisher_container,
                         "processor": processor_container
                     }
+
+
+
+
+
+
+
+
+
+
 
 
