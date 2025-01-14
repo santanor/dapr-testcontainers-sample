de414b26d735c75978afc41a3163b49edbe85b46
Jose Antonio <josementi@gmail.com>
2025-01-14 18:13:52 +0100

    Add a sample

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -1,7 +1,11 @@
 import requests
 import os
 
-def test_order_publisher(containers, base_publisher_url):
-    redis = containers["redis"]
-    
+def test_order_publisher(base_publisher_url, redis, publisher):
+    client = redis.get_client()
+    result = client.xread("{orders}", 0)
+    assert(len(result), 0)
+
+
+
     pass
