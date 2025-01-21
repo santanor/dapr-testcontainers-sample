1fbc78d550a5da62feabb43b8ba9de172eb0b8f1
Jose Antonio <josementi@gmail.com>
2025-01-21 14:51:45 +0000

    Working testcontainer integration tests (#1)
    Working implementation of the dapr test sample using testcontainers

--- order-publisher/app.py
+++ order-publisher/app.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Request
 import json
-import uvicorn
 from dapr.clients import DaprClient
 
 app = FastAPI()
@@ -8,6 +7,9 @@ app = FastAPI()
 @app.post("/order")
 async def publish_order(request: Request):
     order_data = await request.json()
+
+    # Starting the client here uses the default values and the ones in the environment to 
+    # create the client, Therefore there's no extra config here.
     with DaprClient() as client:
         client.publish_event(
             pubsub_name='orders-pubsub',
@@ -21,7 +23,3 @@ async def publish_order(request: Request):
 def check_health():
     """Dapr calls this function to check if the service is ready"""
     return {"status": "healthy"}
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,7 @@
-import os
-from concurrent.futures import ThreadPoolExecutor, wait
-
 import pytest
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.image import DockerImage
+from testcontainers.core.waiting_utils import wait_for_logs
 from testcontainers.core.network import Network
 from testcontainers.redis import RedisContainer
 
@@ -12,42 +10,102 @@ publisher_base = "publisher:latest"
 processor_base = "processor:latest"
 publisher = "publisher:integration"
 processor = "processor:integration"
-
+dapr = "dapr:integration"
 
 @pytest.fixture
 def base_publisher_url():
     return "http://localhost:8000"
 
-
 @pytest.fixture(scope="session", autouse=True)
-def images(request):
-    # Build the images in parallel to save time
-    def create_docker_image(path: str, tag: str) -> DockerImage:
-        return DockerImage(path=path, tag=tag).build()
+def images():
+    # This is just a helper function to simplify the code
+    def create_docker_image(path: str, tag: str, buildargs: dict = None) -> DockerImage:
+        return DockerImage(path=path, tag=tag).build(buildargs=buildargs)
 
     # Build the base images as they'd be deployed in production
     create_docker_image("./order-processor", processor_base)
     create_docker_image("./order-publisher", publisher_base)
-
+ 
     # This uses the base images and extends them to include test-specific dependencies. In this case... just Dapr
     # but it could also include other things such as az cli or test volumes for sample payloads
-    create_docker_image("docker-images/order-processor", processor)
-    create_docker_image("docker-images/order-publisher", publisher)
+    create_docker_image("./tests/docker-images/dapr", dapr)
+    create_docker_image("./tests/docker-images/orders", processor, buildargs={"image": processor_base})
+    create_docker_image("./tests/docker-images/orders", publisher, buildargs={"image": publisher_base})
 
 
-@pytest.fixture
-def containers(images):
-    # This function structure showcases the workflow of the test containers. We can see how the dependencies are passed
-    # "top down" with the network at the root. When the execution finishes this entire stack of nested 'withs' will
-    # unravel and free up resources in order, essentially cleaning up the test environment for a new run.
+@pytest.fixture(scope="function") 
+def network():
     with Network() as network:
-        with (RedisContainer(image="redis:7.4.2-alpine").with_network(network).with_name("redis").with_bind_ports(6379, 6380)) as redis_container:
-            with (DockerContainer(publisher).with_network(network).with_name(publisher).with_bind_ports(8000,8000)) as publisher_container:
-                with (DockerContainer(processor).with_network(network).with_name(processor)) as processor_container:
-                    yield {
-                        "redis": redis_container,
-                        "publisher": publisher_container,
-                        "processor": processor_container
-                    }
+        yield network
+
+@pytest.fixture(scope="function")
+def redis(network):
+    with (RedisContainer(image="redis:7.4.2-alpine").with_network(network).with_name("redis-integration").with_bind_ports(6379, 6380)) as redis_container:
+        yield redis_container
+
+@pytest.fixture(scope="function")
+def processor_container(network):
+    with (DockerContainer(processor)
+        .with_network(network)
+        .with_name("processor")
+        .with_bind_ports(8001, 8001)
+        .with_env("app_id", "order-processor")
+        .with_env("port", "8001")
+        .with_env("dapr_http_port", "3501")
+        .with_env("dapr_grpc_port", "50002")
+        ) as processor_container:
+        
+        # Wait for the application to start. There are many ways to do this, but checking the logs seems simple enough to me
+        wait_for_logs(processor_container, "You're up and running! Both Dapr and your app logs will appear here.")
+
+        yield processor_container
+
+
+@pytest.fixture(scope="function")
+def publisher_container(network):
+    with (DockerContainer(publisher)
+        .with_network(network)
+        .with_name("publisher")
+        .with_bind_ports(8000, 8000)
+        .with_env("app_id", "order-publisher")
+        .with_env("port", "8000")
+        .with_env("dapr_http_port", "3500")
+        .with_env("dapr_grpc_port", "50001")
+        ) as publisher_container:
+        
+        # Wait for the application to start. There are many ways to do this, but checking the logs seems simple enough to me
+        wait_for_logs(publisher_container, "You're up and running! Both Dapr and your app logs will appear here.")
+
+        yield publisher_container
+
+
+
+
+# This pseudo code aims to showcase the order in which containers are created and used. Here we can see how the 
+# network is created first, then redis and then both applications. Once the tests are finished the cleanup happens
+# automatically (thanks to the 'with') but in reverse, first the apps, then Redis and lastly the network. 
+# 
+# We can also see how the network is passed down to every container so that they're all isolated from the local environment
+# @pytest.fixture(scope="function")
+# def containers(request, images):
+#     with Network() as network:
+#         with (RedisContainer(image="redis:7.4.2-alpine").with_network(network) as redis_container:
+#             with (DockerContainer(publisher).with_network(network) as publisher_container:
+#                 with (DockerContainer(processor).with_network(network) as processor_container:
+#                     yield {
+#                         "redis": redis_container,
+#                         "publisher": publisher_container,
+#                         "processor": processor_container
+#                     }
+
+
+
+
+
+
+
+
+
+
 
 
